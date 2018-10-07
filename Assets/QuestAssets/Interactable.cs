ab842becabe2c8cccb996b751390c10b473c727d
vlandemart <vlandemart@gmail.com>
2018-10-08 00:00:12 +0300

    Quest gamemode setup
    Few scripts, materials and one test scene for simple Quest testing

---
+++ Assets/QuestAssets/Interactable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Interactable : MonoBehaviour 
+{
+    public bool interactable = true;
+    protected bool interacted = false;
+    protected PlayerInteractions player = null;
+    [SerializeField]
+    protected UnityEvent[] actions;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interact function is not set-up.");
+        for (int i = 0; i < actions.Length; i++)
+        {
+            actions[i].Invoke();
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        player = other.GetComponent<PlayerInteractions>();
+        if (interactable && player != null)
+        {
+            interacted = true;
+            player.actions += Interact;
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        if (interacted)
+            player.actions -= Interact;
+    }
+}
---
+++ Assets/QuestAssets/PlayerInteractions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour {
+
+    public delegate void Actions();
+    public Actions actions;
+	
+	void Update () {
+		if (Input.GetKeyDown("e"))
+        {
+            if (actions != null)
+                actions();
+        }
+    }
+}
