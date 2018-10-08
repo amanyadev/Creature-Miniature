2bcb4cc5db1c6fcb35af9945eb65b82fddd958a5
vlandemart <vlandemart@gmail.com>
2018-10-08 22:34:19 +0300

    Update #1
    -Added wire script to easy set-up wires.
    -Changed cameraRoller and the way it's called in scene
    -Extended testLevel for one room

---
+++ Assets/QuestAssets/Scripts/Misc.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Misc : MonoBehaviour {
+
+    public void ToggleEnabled()
+    {
+        if (gameObject.activeInHierarchy)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+}
---
+++ Assets/QuestAssets/Scripts/Wire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Wire : MonoBehaviour {
+
+    private LineRenderer lr = null;
+    private Vector3[] children;
+
+    [ContextMenu("BuildWire")]
+	void Build () {
+        children = new Vector3[transform.childCount];
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = children.Length;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i).position;
+            lr.SetPosition(i, children[i]);
+        }
+        lr.SetPositions(children);
+    }
+}
