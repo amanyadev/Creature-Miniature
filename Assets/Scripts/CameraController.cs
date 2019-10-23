9731bd1aba8f0eee9c779fcab3c793b14bc502e5
Quickz <alexserg@inbox.lv>
2019-10-24 01:25:18 +0300

    added a camera controller, made the camera follow the player in sample scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 20f;
+
+    [SerializeField]
+    private bool smoothFollow = true;
+
+    [SerializeField]
+    private GameObject targetToFollow;
+
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - targetToFollow.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = targetToFollow.transform.position + offset;
+        if (smoothFollow)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                newPosition,
+                movementSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            transform.position = newPosition;
+        }
+    }
+}
